87e97e354d876b1f5c375bb437fe677495d1e72b
李晓娟 <lixiaojuan001@lianjia.com>
2018-06-14 11:38:49 +0800

    new folder

---
+++ src/test.js
@@ -0,0 +1,19 @@
+{
+  "name": "gulplearn",
+  "version": "1.0.0",
+  "description": "learn docs",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/sylvialee/gulp-learn.git"
+  },
+  "author": "sylvia",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/sylvialee/gulp-learn/issues"
+  },
+  "homepage": "https://github.com/sylvialee/gulp-learn#readme"
+}
